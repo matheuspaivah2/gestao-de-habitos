99869eee614346ae1cc4233f6bd7d5b6c9e9aabd
gustavovieirazacchi <you gustavovieirazacchi@gmail.com>
2021-06-09 11:11:59 -0300

    feat: Criada tela de SignUp

--- src/pages/Singup/index.js
+++ src/pages/Singup/index.js
@@ -1,9 +1,50 @@
+import{Link,useHistory} from "react-router-dom"
+import Button from "../../components/Button"
+import Input from "../../components/Input"
+import {Container,AnimationContainer} from "./styles"
+import {FiVoicemail, FiLock,FiUser} from "react-icons/fi"
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import api from "../../services/api"
+import{ toast } from 'react-toastify'
 
 const Singup = () => {
 
-    return (
-     <h1>Teste Sing Up</h1>
-    );
-  };
-  
-  export default Singup;
+    const forSchema = yup.object().shape({
+        username: yup.string().required("Username Obrigatótio"),
+        email: yup.string().required("Email Obrigatótio").email("Email Inválido"),
+        password: yup.string().required("Campo Obrigatório"),
+      })
+    const{register, handleSubmit, formState:{errors}} = useForm({
+        resolver: yupResolver(forSchema)
+    });
+    const history = useHistory()
+
+
+    const handleSubmitFunction = (data) => {
+        api.post("/users/", data)
+        .then((response)=> {
+            const {access} = response.data
+            localStorage.setItem("@GestãoDeHábitos:access", JSON.stringify(access))
+            return history.push('/login')
+        })
+        .catch((err) => toast.error('Erro ao criar usuário'))
+    }
+
+
+    return(
+        <Container>
+            <AnimationContainer>
+              <form onSubmit={handleSubmit(handleSubmitFunction)}>
+                    <h1>Register</h1>
+                    <Input register={register} name="username" icon={FiUser} placeholder="Seu Username" error={errors.username?.message}/>
+                    <Input register={register} name="email" icon={FiVoicemail} placeholder="Seu Email" error={errors.email?.message}/>
+                    <Input register={register} name="password" icon={FiLock} placeholder="Sua Senha" type="password" error={errors.password?.message}/>
+                    <Button type="submit" size={true}>Cadastrar</Button>
+              </form>
+            </AnimationContainer>
+        </Container>
+    )
+}
+export default Singup
