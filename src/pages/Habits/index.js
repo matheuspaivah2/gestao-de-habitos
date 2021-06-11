1a91365f0aef9e41ca79ea371e809691d5185434
Leonardo Lima <leoexori@hotmail.com>
2021-06-11 08:50:54 -0300

    form cadastro novo habito sem requisicao para a api

--- src/pages/Habits/index.js
+++ src/pages/Habits/index.js
@@ -1,28 +1,24 @@
-import { useState } from "react";
 import api from "../../services/api"
 import * as yup from 'yup'
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Grid, TextField, Button, Switch, FormControlLabel} from '@material-ui/core'
-import Container, {AnimationContainer} from "./styles";
+import { Button, MenuItem, Menu } from '@material-ui/core'
+import Container, {AnimationContainer, Box, InputMenu, BackgroundHeading} from "./styles";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Input from '../../components/Input'
+import {toast} from 'react-toastify'
+import { useState } from "react";
 
 const Habits = () => {
   const [data, setData] = useState([])
-  const [check, setCheck] = useState({
-    checkedA: true,
-  });
-
-  const handleChange = (event) => {
-    setCheck({ ...check, [event.target.name]: event.target.checked });
-    console.log(check.checkedA)
-  };
+  const [anchorEl, setAnchorEl] = useState(null)
 
   const formSchema = yup.object().shape({
     title: yup.string().required("Campo obrigatório"),
     category: yup.string().required("Campo obrigatório"),
     difficulty: yup.string().required("Campo obrigatório"),
     frequency: yup.string().required("Campo obrigatório"),
-    achieved: yup.boolean().required(" aaaCampo Obrigatório"),
+    achieved: yup.boolean().required("Campo Obrigatório"),
     how_much_achieved: yup.number().required("Campo obrigatório"),
     user: yup.number().required()
   })
@@ -34,32 +30,68 @@ const Habits = () => {
   const onSubmitFunc = (data) => {
     setData(data)
     console.log(data)
-    api.post("/habits/", data).then(res => console.log(res.data))
+    api.post("/habits/", data).then((res) => { 
+      console.log(res.data)
+      const {access} = res.data
+      console.log(access)
+      // let decoded = jwt_decode(access);
+      window.localStorage.clear()
+      window.localStorage.getItem()
+    })
+    .catch((err) => toast.error('Erro ao criar habito'))
   }
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
     return (
+    <>
+    <BackgroundHeading>
+      <h1>habits</h1>
+    </BackgroundHeading>
       <Container>
         <AnimationContainer>
-          <form onSubmit={handleSubmit(onSubmitFunc)}>
-                  <TextField {...register("title")} label="Habito" variant="filled" required/>
-                  {errors.title?.message}
-                  <TextField {...register("category")} label="Categoria" variant="filled" required/>
-                  {errors.category?.message}
-                  <TextField {...register("difficulty")} label="Dificuldade" variant="filled" required/>
-                  {errors.difficulty?.message}
-                  <TextField {...register("frequency")} label="Frequencia" variant="filled" required/>
           
-                  <FormControlLabel {...register("achieved")}  control={<Switch checked={check.checkedA} onChange={handleChange} name="checkedA" />}
-                  label="Meta alcancada" value={check.checkedA}/>
-                  {/* <TextField {...register("achieved")} label="meta alcancada" /> */}
-                  {errors.frequency?.message}
-                  {errors.achieved?.message}
-                  <TextField {...register("how_much_achieved")} label="Porcentagem alcancada" variant="filled" required/>
-                  <TextField {...register("user")} label="user" variant="filled" required/>
-                {errors.address?.message}
-                  <Button variant="outlined" type="submit">Registrar</Button>
-          </form>
+            <Box>
+              <InputMenu>
+              <Button aria-controls={'menu-list-grow'} aria-haspopup="tree" onClick={handleClick}>
+                Criar Novo Habito 
+              </Button>
+              <ExpandMoreIcon onClick={handleClick}/>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}>
+                <MenuItem style={{display: "flex", flexDirection: "column"}}>
+                  <form onSubmit={handleSubmit(onSubmitFunc)}>
+                  <Input name="title" placeholder="Hábito" required register={register} error={errors.title?.message}/>
+                  <Input name="category" placeholder="Categoria" required register={register} error={errors.title?.message}/>
+                  <Input name="difficulty" placeholder="Dificuldade" required register={register} error={errors.title?.message}/>
+                  <Input name="frequency" placeholder="Frequencia" required register={register} error={errors.title?.message}/>
+                  <Input name="achieved" placeholder="Frequencia" required register={register} error={errors.title?.message}/>
+                  <Input name="how_much_achieved" placeholder="Frequencia" required register={register} error={errors.title?.message}/>
+                  <Input name="user" placeholder="Frequencia" required register={register} error={errors.title?.message}/>
+                  <MenuItem style={{display: "flex", justifyContent: "center"}}>
+                  <Button type="submit">Ok</Button>
+                  <Button onClick={handleClose}>Cancelar</Button>
+                  </MenuItem>
+                </form>
+                </MenuItem>
+                
+              </Menu>
+              </InputMenu>
+              </Box>
+          
         </AnimationContainer>
       </Container>
+    </>
     );
   };
   
--- src/pages/Habits/styles.js
+++ src/pages/Habits/styles.js
@@ -1,9 +1,8 @@
 import styled, { keyframes } from "styled-components"
 
 const Container = styled.div`
-  height:100vh;
+  margin-top: 20px;
   display:flex;
-  align-items: center;
   justify-content: center;
   text-align: center;
   flex-direction: column;
@@ -27,6 +26,57 @@ export const AnimationContainer = styled.div`
     align-items: center;
     justify-content:center;
     animation: ${appearFromRight} 1s;
-`
+    form{
+        width:250px;
+        text-align:center;
+    }
+    h1{
+        margin-bottom:32px;
+    }
+    p{
+        margin-top:8px;
+        a{
+            font-weight: bold;
+            color: var(--orange);
+        }
+    }
+`;
+
+export const Box = styled.div`
+  border: 2px solid #000;
+  border-radius: 5px;
+  margin: 10px;
+`;
+
+export const InputMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--white);
+  border-radius: 1px;
+  border: 2px solid var(--black);
+  color: var(--gray);
+  padding: 0.7rem;
+  width: 100%;
+  Button {
+    font-size: 12px;
+    display: flex;
+    
+    text-align: center;
+  }
+  Menu {
+    border: solid 2px black;
+  }
+`;
+
+export const BackgroundHeading = styled.div`
+  background-color: #3AACB6;
+  border-radius: 0 0 30px 30px;
+  width: 200px;
+  margin: 0 auto;
+  h1 {
+    color: #fff;
+  }
+`;
 
 export default Container;
