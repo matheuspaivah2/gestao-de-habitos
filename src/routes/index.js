d61b96ac124ddef394abcde8f20385be4e2186ad
gustavovieirazacchi <you gustavovieirazacchi@gmail.com>
2021-06-11 00:11:38 -0300

    feat: Criada PageNotFound

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import SignUp from "../pages/SignUp";
 import Groups from "../pages/Groups";
 import Habits from "../pages/Habits";
 import MyAcount from "../pages/MyAcount";
+import PageNotFound from "../pages/NotFound";
 
 const Routes = () => {
   return (
@@ -18,6 +19,7 @@ const Routes = () => {
       <Route path="/groups" component={Groups} />
       <Route path="/habits" component={Habits} />
       <Route path="/myacount" component={MyAcount} />
+      <Route component={PageNotFound} />
     </Switch>
   );
 };
