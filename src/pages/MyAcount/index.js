ca2591d759f591d38f18d52050b572e877e0e075
gustavovieirazacchi <you gustavovieirazacchi@gmail.com>
2021-06-14 10:26:40 -0300

    fix: adicionando editar email e estado atual do usuario

--- src/pages/MyAcount/index.js
+++ src/pages/MyAcount/index.js
@@ -1,41 +1,58 @@
 import Button from "../../components/Button"
 import Input from "../../components/Input"
 import {Container,AnimationContainer} from "./styles"
-import {FiUser} from "react-icons/fi"
+import {FiUser, FiVoicemail} from "react-icons/fi"
 import * as yup from 'yup';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import api from "../../services/api"
 import{ toast } from 'react-toastify'
 import jwt_decode from "jwt-decode"
+import {useEffect,useState} from 'react'
 
 
 const MyAcount = () => {
-
+    const [usuario, setUsuario] = useState('');
     const forSchema = yup.object().shape({
         username: yup.string().required("Username Obrigatótio"),
+        email: yup.string().required("email Obrigatótio")
       })
     const{register, handleSubmit, formState:{errors}} = useForm({
         resolver: yupResolver(forSchema)
     });  
-
+    
     const handleSubmitFunction = (data) => {
         const token = JSON.parse(localStorage.getItem("@GestãoDeHábitos:access"))
         var decode = jwt_decode(token)
         api.patch(`/users/${decode.user_id}/`, data, {headers:{Authorization: `Bearer ${token}`}})
-        .then(()=> {toast.success('Sucesso ao editar usuário')})
+        .then(()=> {toast.success(`Seu email agora é ${data.email}`)})
         .then(()=> {toast.success(`Seu usuário agora é ${data.username}`)})
+        .then(()=>submitFunction())
         .catch((err) => toast.error('Erro ao editar usuário'))
     }
+    const submitFunction = () => {
+        const token = JSON.parse(localStorage.getItem("@GestãoDeHábitos:access"))
+        var decode = jwt_decode(token)
+        api.get(`/users/${decode.user_id}/`, {headers:{Authorization: `Bearer ${token}`}})
+        .then((response)=> setUsuario(response.data))
+        .then((response)=> console.log(usuario))
+
+        .catch((err) => toast.error('Erro ao exibir usuário'))
+    }
+    useEffect(() => {submitFunction()}, [])
 
 
     return(
         <Container>
+            <h1>Alterar Usuário</h1>
             <AnimationContainer>
               <form onSubmit={handleSubmit(handleSubmitFunction)}>
-                    <h1>Alterar Usuário</h1>
+                    
+                    <h3>Username atual: {usuario.username}</h3>
+                    <h3>Email atual: {usuario.email}</h3>
                     <Input register={register} name="username" icon={FiUser} placeholder="Seu Novo Username" error={errors.username?.message}/>
-                    <Button type="submit" size={true}>Alterar</Button>
+                    <Input register={register} name="email" icon={FiVoicemail} placeholder="Seu Novo Email" error={errors.email?.message}/>
+                    <Button type="submit" size={"90%"}>Alterar</Button>
               </form>
             </AnimationContainer>
         </Container>
