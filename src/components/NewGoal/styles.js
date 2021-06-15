2b236a37a2723b37f0cb162f5557c85e4cd955d7
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-06-15 09:51:58 -0300

    feat: MyGroups, modais de register e delete para goals e activities, modal para criação de novo grupo, link com a api

---
+++ src/components/NewGoal/styles.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import { TextField, withStyles } from "@material-ui/core";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* margin: 0 auto; */
+  background-color: white;
+  width: 100%;
+  height: 60vh;
+  padding: 0;
+
+  .container__nameGoal {
+    margin: 0;
+    height: 9vh;
+    width: 100%;
+    background-color: #056479;
+    /* border-bottom-left-radius: 100px;
+    border-bottom-right-radius: 100px; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 2px solid black;
+    strong {
+      font-size: 1.5rem;
+      color: white;
+      font-weight: bolder;
+    }
+  }
+
+  .container__inputs {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2vh;
+    row-gap: 2vh;
+    padding: 4px;
+    height: 81%;
+
+    h2 {
+      text-transform: uppercase;
+      font-size: 1rem;
+      text-align: center;
+    }
+  }
+
+  .container__buttons {
+    width: 100%;
+    display: flex;
+    column-gap: 3vw;
+    justify-content: center;
+    align-items: center;
+    margin-top: auto;
+    margin-bottom: 1vh;
+
+    button {
+      width: 40%;
+      border: none;
+      background-color: #056479;
+      border-radius: 4px;
+      color: white;
+      font-size: 1rem;
+      font-weight: bolder;
+      padding: 8px;
+    }
+  }
+`;
+
+export const Input = withStyles({
+  root: {},
+})(TextField);
+
+export const HiddenInput = withStyles({
+  root: {
+    display: "none",
+  },
+})(TextField);
